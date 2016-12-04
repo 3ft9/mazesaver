4017426cec573f530f229a69ddebf375dbbb2264
Stut <stuart@stut.net>
2016-12-04 19:28:52 +0000

    Initial Commit

---
+++ mazesaver/mazesaverView.m
@@ -0,0 +1,52 @@
+//
+//  mazesaverView.m
+//  mazesaver
+//
+//  Created by Stuart Dallas on 04/12/2016.
+//  Copyright © 2016 3ft9 Ltd. All rights reserved.
+//
+
+#import "mazesaverView.h"
+
+@implementation mazesaverView
+
+- (instancetype)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
